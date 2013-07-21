75a9e7339c584a554114aa6f757f8d4cbd904f4b
Dai Yamamoto <gos415345@gmail.com>
2013-07-21 11:33:41 +0900

    追加し忘れてました

---
+++ ITSP_Server/src/jp/ac/titech/itpro/sds/fragile/api/dto/ScheduleV1Dto.java
@@ -0,0 +1,29 @@
+package jp.ac.titech.itpro.sds.fragile.api.dto;
+
+import javax.persistence.Entity;
+
+
+@Entity
+public class ScheduleV1Dto {
+    private long startTime;
+    private long finishTime;
+    
+    public ScheduleV1Dto(long startTime, long finishTime) {
+        this.setStartTime(startTime);
+        this.setFinishTime(finishTime);
+    }
+    
+    public long getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+    public long getFinishTime() {
+        return finishTime;
+    }
+    public void setFinishTime(long finishTime) {
+        this.finishTime = finishTime;
+    }
+    
+}
