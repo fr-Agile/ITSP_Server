34afb4905369a5ce64dbaa8a8911d7de7a9d4a54
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-08-17 10:41:29 +0900

    コンテナ移植

---
+++ ITSP_Server/src/jp/ac/titech/itpro/sds/fragile/api/container/StringListContainer.java
@@ -0,0 +1,18 @@
+package jp.ac.titech.itpro.sds.fragile.api.container;
+
+import java.util.List;
+
+public class StringListContainer {
+    private List<String> list;
+
+    public List<String> getList() {
+        return list;
+    }
+
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+    
+   
+
+}
