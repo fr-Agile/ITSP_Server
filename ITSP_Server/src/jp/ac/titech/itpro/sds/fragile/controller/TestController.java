3cdcecc5fa15274180ae8208563f422672f60495
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-11-10 11:05:15 +0900

    .gitignore is now working

---
+++ ITSP_Server/src/jp/ac/titech/itpro/sds/fragile/controller/TestController.java
@@ -0,0 +1,31 @@
+package jp.ac.titech.itpro.sds.fragile.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import jp.ac.titech.itpro.sds.fragile.model.User;
+import jp.ac.titech.itpro.sds.fragile.service.ScheduleService;
+import jp.ac.titech.itpro.sds.fragile.service.UserService;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+
+public class TestController extends Controller {
+
+    @Override
+    public Navigation run() throws Exception {
+        User user = UserService.createUser("test", "test", "a@a.com", "test");
+        Map<String, Object> map = new HashMap<String, Object>();
+        Date day = new Date();
+        map.put("startTime", day.getTime());
+        map.put("finishTime", day.getTime() + 1000 * 60 * 30);
+        ScheduleService.createSchedule(map, user);
+        Map<String, Object> map2 = new HashMap<String, Object>();
+        map2.put("startTime", day.getTime() + 1000 * 60 * 60);
+        map2.put("finishTime", day.getTime() + 1000 * 60 * 150);
+        ScheduleService.createSchedule(map2, user);
+        return forward("test.jsp");
+    }
+}
